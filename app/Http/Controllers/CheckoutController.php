425f506e99e48d228709e24745328852640daf80
khoa <khoauth@gmail.com>
2023-12-30 07:08:01 +0700

    edit request

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -70,7 +70,7 @@ class CheckoutController extends Controller
             'total_price' => $request->total_price,
             'shipping_address' => $request->shipping_address,
             'billing_address' => $request->billing_address,
-            'payment_method' => $request->radio ?? $request->payment_method,
+            'payment_method' => $request->payment_method,
             'status' => 0,
         ]);
         foreach ($cart as $item) {
